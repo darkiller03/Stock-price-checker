f425f7f39002fb2f0f28b2e45a3e601ba36648ab
Karim Hssini <hssinikarim2003@gmail.com>
2024-09-25 11:09:03 +0100

    commit

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,68 @@
 'use strict';
+const StockModel = require("../models").Stock;
+const fetch = require("node-fetch");
+const { Stock } = require("../models");
+
+async function createStock(stock,like,ip){
+  const newStock = new StockModel({
+    symbol:stock,
+    likes: like ? [ip] : [],
+  });
+  const savedNew = await newStock.save();
+  return savedNew;
+}
+
+async function fincStock(stock){
+  return await StockModel.findOne({symbol:stock}).exec();
+
+}
+
+async function saveStock(stock,like,ip) {
+  let saved = {};
+  const foundStock = await findStock(stock);
+  if(!foundStock){
+    const createsaved = await createStock(stock,like,ip);
+    saved = createsaved;
+    return saved;
+  } else{
+    if(like && foundStock.likes.indexOf(ip) === -1){
+      foundStock.likes.push(ip);
+    }
+    saved = await foundStock.save();
+    return saved;
+  }
+
+
+}
+
+async function getStock(stock){
+  const response = await fetch(
+    `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+  );
+  const { symbol, latestPrice} = await response.json();
+  return { symbol, latestPrice };
+}
 
 module.exports = function (app) {
+  //https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/TSLA/quote
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+  app.route('/api/stock-prices').get(async function(req, res){
+    const { stock, like } = req.query;
+    const {symbol , latestPrice } = await getStock(stock);
+    if (!symbol){
+      res.json({stockData: {likes : like ? 1 : 0}});
+      return ;
+    }
+
+    const oneStockData = await saveStock(symbol,like,ip);
+    console.log("One Stock Data",oneStockData);
+
+    res.json({
+      stockData: {
+        stock: symbol,
+        price: latestPrice,
+        likes: oneStockData.likes.length,
+      },
     });
-    
+  });
 };
