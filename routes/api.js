51f97da67c5ead4228d31742cd1cc9bf53afc39f
Karim Hssini <hssinikarim2003@gmail.com>
2024-09-25 11:29:43 +0100

    fixed api.js for compare function

--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,7 @@ async function createStock(stock,like,ip){
   return savedNew;
 }
 
-async function fincStock(stock){
+async function findStock(stock){
   return await StockModel.findOne({symbol:stock}).exec();
 
 }
@@ -48,13 +48,58 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices').get(async function(req, res){
     const { stock, like } = req.query;
+    if (Array.isArray(stock)){
+      console.log("stocks",stock);
+      
+      const {symbol, latestPrice} = await getStock(stock[0]);
+      const {symbol: symbol2, latestPrice: latestPrice2} = await getStock(
+        stock[1]
+      );
+
+      const firststock = await saveStock(stock[0], like, req.ip);
+      const secondstock = await saveStock(stock[1],like, req.ip);
+
+      let stockData = [];
+      if(!symbol){
+        stock.Data.push({
+          rel_likes: firststock.likes.length - secondstock.likes.length,
+        });
+      } else {
+        stockData.push({
+          stock: symbol,
+          price: latestPrice,
+          rel_likes: firststock.likes.length - secondstock.likes.length,
+      });
+      }
+
+      if(!symbol2){
+        stock.Data.push({
+          rel_likes: secondstock.likes.length - firststock.likes.length,
+        });
+      } else {
+        stockData.push({
+          stock: symbol2,
+          price: latestPrice2,
+          rel_likes: secondstock.likes.length - firststock.likes.length,
+      });
+      }
+      
+      res.json({
+        stockData,
+      });
+      return;
+
+
+
+
+    }
     const {symbol , latestPrice } = await getStock(stock);
     if (!symbol){
       res.json({stockData: {likes : like ? 1 : 0}});
       return ;
     }
 
-    const oneStockData = await saveStock(symbol,like,ip);
+    const oneStockData = await saveStock(symbol,like, req.ip);
     console.log("One Stock Data",oneStockData);
 
     res.json({
